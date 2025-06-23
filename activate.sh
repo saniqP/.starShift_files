58777a477dc0ebf8974440e9a9415b6cf7337afc
saniqP <undertalesyt@gmail.com>
2025-06-23 22:48:20 +0300

    Add files via upload

---
+++ activate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+current_path=$(pwd)
+cleaned_path=$(echo "$current_path" | sed 's|/activate.sh||g')
+
+new_path="$cleaned_path/files:$PATH"
+
+
+export PATH="$new_path"
+
+
+if ! grep -q "export PATH=\"$cleaned_path/files:\$PATH\"" ~/.bashrc; then
+    echo "export PATH=\"$cleaned_path/files:\$PATH\"" >> ~/.bashrc
+    echo "PATH added in ~/.bashrc"
+fi
+
+rm activate.sh
